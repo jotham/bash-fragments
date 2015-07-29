5e65366054b48d010f357b8171519ec5ee626e71
Jotham Read <jotham.read@gmail.com>
2015-07-29 18:07:41 +1200

    Added script-template.sh

---
+++ script-template.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## Usage: script-template.sh [options]
+##
+##    options
+##
+##       -e --example            Run example program
+##       -h                      This text
+##
+
+BASE=$(cd $(dirname $0); pwd -P)
+
+usage() {
+   echo "$(grep "^## " "${BASH_SOURCE[0]}" | cut -c 4-)"
+   exit 0
+}
+
+warn() {
+   cat <<< "$@" 1>&2
+}
+
+error() {
+   cat <<< "$@" 1>&2
+   exit 1
+}
+
+[[ $# == 0 ]] && usage
+
+example() {
+   warn "This is just an example."
+}
+
+EXAMPLE=
+
+while [[ $# > 0 ]]; do
+   key="$1"
+   case $key in
+      -h|--help)
+         usage
+         ;;
+      -e|--example)
+         EXAMPLE=1
+         ;;
+   esac
+   shift
+done
+
+if [[ -n "$EXAMPLE" ]]; then
+   example
+fi
+
