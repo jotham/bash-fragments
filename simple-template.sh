7d1d3cae38af5c80ffc647195caf427a3b8e2942
Jotham Read <jotham.read@gmail.com>
2015-07-29 18:17:32 +1200

    Tidyup

--- simple-template.sh
+++ simple-template.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Script that requires one parameter to run
+
 error() {
    cat <<< "$@" 1>&2
    exit 1
 }
 
-[[ $# == 0 ]] && error "Usage: $(basename $0) <URL>"
+[[ $# == 0 ]] && error "Usage: $(basename $0) <something>"
 
+printf "Running with %s.\n" "$1"
